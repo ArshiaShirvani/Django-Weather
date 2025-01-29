7dbbc123d76e0f30328d33d493bc511ee7df848e
Arshia <arshiashirvani.f1385@gmail.com>
2025-01-29 09:30:42 +0330

    ignore the api_key of the openweathermap site

--- weather/views.py
+++ weather/views.py
@@ -2,17 +2,17 @@ import requests
 from django.shortcuts import render
 
 def weather_view(request):
-    city = ''  # پیش‌فرض نام شهری انتخاب نشده
+    city = '' 
     temperature = None
     description = ''
     icon = ''
 
-    if request.method == 'POST':  # اگر فرم ارسال شده باشد
-        city = request.POST.get('city', 'Tehran')  # مقدار پیش‌فرض: تهران
-        api_key = '0642217953ecf5f2cc625a73bbb46cb1'  # کلید API خودتان از OpenWeatherMap
+    if request.method == 'POST': 
+        city = request.POST.get('city', 'Tehran')  
+        api_key = open("API_KEY","r").read()  
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
         response = requests.get(url)
-        if response.status_code == 200:  # درخواست موفقیت‌آمیز
+        if response.status_code == 200:  
             data = response.json()
             temperature = data['main']['temp']
             description = data['weather'][0]['description']
