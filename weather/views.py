dbca49b0f640de35f783fb37834af3fc49032b75
Arshia <arshiashirvani.f1385@gmail.com>
2025-03-31 14:41:05 +0330

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -9,7 +9,7 @@ def weather_view(request):
 
     if request.method == 'POST': 
         city = request.POST.get('city', 'Tehran')  
-        api_key = open("API_KEY","r").read()  
+        api_key = "YOUR API KEY"
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
         response = requests.get(url)
         if response.status_code == 200:  
